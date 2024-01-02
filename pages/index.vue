cd2b70426f7314a5e26c65670ed48428450e1eae
John Halsey <johnhalsey@Johns-MacBook-Pro.local>
2024-01-02 21:36:15 +0000

    tailwind and pages

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<NuxtLink to="/memory-sequence">
+			Memory Game
+		</NuxtLink>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "HomePage"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/memory-sequence.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="underline border p-3">
+		Hello
+	</div>
+</template>
+
+<script>
+export default {
+ 	name: "memory-sequence.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
