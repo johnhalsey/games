cfcbe2728050a7fffca639199747d33a2a6a0462
John Halsey <johnhalsey@Johns-MacBook-Pro.local>
2024-01-09 22:08:44 +0000

    should be working, but isnt

--- pages/memory-sequence.vue
+++ pages/memory-sequence.vue
@@ -3,18 +3,23 @@
 		<div class="border p-3 bg-slate-200">
 			<ul class="w-full md:w-1/2">
 				<li class="inline w-1/3 mr-5">
-					<button class="p-3 bg-blue-200 rounded hover:bg-blue-300">New Game</button>
+					<button class="p-3 bg-blue-200 rounded hover:bg-blue-300"
+									@click="startGame">
+						New Game
+					</button>
 				</li>
 				<li class="inline mr-5 ">High Score: {{ highScore }}</li>
 				<li class="inline mr-5">Level: {{ currentLevel }}</li>
 			</ul>
 		</div>
 		<div class="grow">
-			<div class="h-full grid grid-cols-2 grid-rows-2">
-				<div class="bg-yellow-500"></div>
-				<div class="bg-purple-500"></div>
-				<div class="bg-pink-500"></div>
-				<div class="bg-green-500"></div>
+			<div class="grid h-full"
+					 :class="containerClasses">
+				<div v-for="index in segments"
+						 :key="'segment-' + index"
+						 :class="`bg-${colours[index -1]}-500`">
+			{{ index }}
+				</div>
 			</div>
 		</div>
 	</div>
@@ -26,15 +31,49 @@ export default {
 	data () {
 		return {
 			highScore: 0,
-			currentLevel: 1
+			currentLevel: 1,
+			difficulties: [
+				{label: 'Easy', segments: 4},
+				{label: 'Medium', segments: 6},
+				{label: 'Hard', segments: 8},
+			],
+			currentDifficulty: 'Easy',
+			colours: [
+				'yellow',
+				'purple',
+				'pink',
+				'green',
+				'blue',
+				'lime',
+				'red',
+				'amber'
+			]
 		}
 	},
+	computed: {
+		segments () {
+			return this.difficulties.find(difficulty => difficulty.label === this.currentDifficulty).segments;
+		},
+		containerClasses () {
+			return 'grid-cols-' + this.cols + ' grid-rows-' + this.cols;
+		},
+		cols () {
+			return this.segments / 2;
+		}
+	},
+	methods: {
+		startGame () {
+			console.log("start game");
+		}
+	}
+
 }
 </script>
 
 <style>
 html,
-body {
+body,
+#__nuxt {
 	@apply h-full;
 }
 </style>
