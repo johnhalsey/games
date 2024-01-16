fb3f6d6cd45a96f4b68852e7253c8cab22ed9987
John Halsey <johnhalsey@Johns-MacBook-Pro.local>
2024-01-16 22:08:07 +0000

    Add scoring and high save high score

--- pages/memory-sequence.vue
+++ pages/memory-sequence.vue
@@ -2,14 +2,26 @@
 	<div class="flex flex-col h-full">
 		<div class="border p-3 bg-slate-200">
 			<ul class="w-full md:w-1/2">
-				<li class="inline w-1/3 mr-5">
+				<li class="md:inline w-1/3 mr-5">
 					<button class="p-3 bg-blue-200 rounded hover:bg-blue-300"
 									@click="startGame">
 						New Game
 					</button>
 				</li>
-				<li class="inline mr-5 ">High Score: {{ highScore }}</li>
-				<li class="inline mr-5">Level: {{ currentLevel }}</li>
+				<li class="md:inline mr-5">
+					<select class="p-3 bg-blue-200 rounded w-26 hover:bg-blue-300"
+									v-model="currentDifficulty">
+						<option v-for="difficulty in difficulties"
+										:key="difficulty.label"
+										:value="difficulty.label">
+							{{ difficulty.label }}
+						</option>
+					</select>
+				</li>
+				<li class="md:inline mr-5 ">Current Score: {{ currentScore }}</li>
+				<li class="md:inline mr-5 ">High Score: {{ highScore }}</li>
+				<li class="md:inline mr-5">Level: {{ currentLevel }}</li>
+
 			</ul>
 		</div>
 		<div class="grow">
@@ -41,13 +53,14 @@ export default {
 	data () {
 		return {
 			highScore: 0,
+			currentScore: 0,
 			currentLevel: 1,
 			difficulties: [
 				{label: 'Easy', segments: 4},
 				{label: 'Medium', segments: 6},
 				{label: 'Hard', segments: 9},
 			],
-			currentDifficulty: 'Easy',
+			currentDifficulty: 'Hard',
 			colours: [
 				'green',
 				'blue',
@@ -86,27 +99,41 @@ export default {
 			return rows
 		}
 	},
+	mounted () {
+		this.getHighScore()
+	},
 	methods: {
-
+		getHighScore () {
+			let highScore = 0
+			if (localStorage.getItem('highScore') !== null) {
+				highScore = localStorage.getItem('highScore')
+			}
+			this.highScore = highScore
+		},
 		startGame () {
 			this.sequence = []
 			this.nextTurn()
-		},
-		nextTurn () {
+		}
+		,
+		nextTurn (level = 1) {
 			const segments = this.difficulties.find(difficulty => difficulty.label === this.currentDifficulty).segments;
 			const index = this.randomIntFromInterval(0, segments - 1)
 			this.sequence.push(index)
-			this.play()
-		},
+			this.play(level)
+		}
+		,
 		randomIntFromInterval (min, max) { // min and max included
 			return Math.floor(Math.random() * (max - min + 1) + min)
-		},
-		async play () {
+		}
+		,
+		async play (level) {
+			this.currentLevel = level
 			this.userSequence = []
 			for (const index of this.sequence) {
 				await this.highlight(index);
 			}
-		},
+		}
+		,
 		highlight (index) {
 			return new Promise((resolve) => {
 				this.activeSegment = index
@@ -117,12 +144,28 @@ export default {
 					}, 300)
 				}, 500)
 			})
-		},
+		}
+		,
 		segmentSelected (index) {
 			this.userSequence.push(index)
+			// check if the user has selected the correct sequence
+			if (this.userSequence[this.userSequence.length - 1] !== this.sequence[this.userSequence.length - 1]) {
+				// set the new high score if the current score is higher
+				if (this.currentScore > this.highScore) {
+					this.highScore = this.currentScore
+					// save the high score to local storage
+					localStorage.setItem('highScore', this.currentScore)
+				}
+				alert('Game Over')
+
+				return;
+			} else {
+				this.currentScore++
+			}
+
 			if (this.userSequence.length === this.sequence.length) {
 				setTimeout(() => {
-					this.nextTurn()
+					this.nextTurn(this.currentLevel + 1)
 				}, 500)
 
 			}
